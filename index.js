1415597cb78d8f7f650c8f332229c1e15650ebd3
daviddinh1 <91696365+daviddinh1@users.noreply.github.com>
2024-11-11 14:27:30 -0600

    changed files using express framework

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-const http = require("http");
-const url = require("url");
-const fs = require("fs");
-const path = require("path");
+// const http = require("http");
+// const url = require("url");
+// const fs = require("fs");
+// const path = require("path");
+const express = require("express");
+const app = express();
 
 // Define the path for the HTML files directory
-const baseDir = path.join(__dirname, "public");
+// const baseDir = path.join("./websites", "public");
 
 // Map paths to their corresponding HTML files
 const routes = {
@@ -13,27 +15,62 @@ const routes = {
   "/contact-me": "contact-me.html",
 };
 
-// Create the server
-http
-  .createServer((req, res) => {
-    const parsedUrl = url.parse(req.url, true);
-    const route = routes[parsedUrl.pathname] || "404.html";
-    const filePath = path.join(baseDir, route);
-
-    // Read the HTML file and serve it
-    fs.readFile(filePath, (err, data) => {
+app.get("/", (req, res) => {
+  res.sendFile(
+    "/Users/daviddinh/repos/basic-informational-site/websites/index.html",
+    function (err) {
       if (err) {
-        res.writeHead(500, { "Content-Type": "text/html" });
-        res.end("500 Server Error");
+        console.log(err);
       } else {
-        // Set Content-Type and respond
-        res.writeHead(route === "404.html" ? 404 : 200, {
-          "Content-Type": "text/html",
-        });
-        res.end(data);
+        console.log("index works");
       }
-    });
-  })
-  .listen(8080, () => {
-    console.log("Server running at http://localhost:8080/");
-  });
+    }
+  );
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(
+    "/Users/daviddinh/repos/basic-informational-site/websites/about.html"
+  );
+});
+
+app.get("/contact-me", (req, res) => {
+  res.sendFile(
+    "/Users/daviddinh/repos/basic-informational-site/websites/contact.html"
+  );
+});
+
+app.get("*", function (req, res) {
+  res.sendfile(
+    "/Users/daviddinh/repos/basic-informational-site/websites/404.html"
+  );
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`My first Express app - listening on port ${PORT}!`);
+});
+// Create the server
+// http
+//   .createServer((req, res) => {
+//     const parsedUrl = url.parse(req.url, true);
+//     const route = routes[parsedUrl.pathname] || "404.html";
+//     const filePath = path.join(baseDir, route);
+
+//     // Read the HTML file and serve it
+//     fs.readFile(filePath, (err, data) => {
+//       if (err) {
+//         res.writeHead(500, { "Content-Type": "text/html" });
+//         res.end("500 Server Error");
+//       } else {
+//         // Set Content-Type and respond
+//         res.writeHead(route === "404.html" ? 404 : 200, {
+//           "Content-Type": "text/html",
+//         });
+//         res.end(data);
+//       }
+//     });
+//   })
+//   .listen(8080, () => {
+//     console.log("Server running at http://localhost:8080/");
+//   });
